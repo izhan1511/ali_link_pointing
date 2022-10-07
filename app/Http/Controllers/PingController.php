26209ea25f2166d4cb1fabbcf3adf73d9c3e8c66
izhan1511 <izhan47@gmail.com>
2022-10-07 20:31:06 +0500

    Ping Ip. Database queue job

--- app/Http/Controllers/PingController.php
+++ app/Http/Controllers/PingController.php
@@ -2,12 +2,37 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\ip;
+use App\Jobs\PingIpJob;
 
 class PingController extends Controller
 {
-    public function serverStatus(Request $request)
+    public function pingIp()
     {
-        
+        $first = rand(0, 255);
+        $sec = rand(0, 255);
+        $third = rand(0, 255);
+        $fourth = rand(0, 255);
+        $ip = $first . '.' . $sec . '.' . $third . '.' . $fourth;
+
+        $ch = curl_init($ip);
+        // $ch = curl_init('1.1.1.1');
+        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $data = curl_exec($ch);
+        $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        if ($data !== false) {
+            $ip = ip::create([
+                'ip' => $ip,
+            ]);
+        } else {
+            // \Log::error('IP: ' . $ip);
+            // \Log::error('data: ' . json_encode($data));
+        }
+        PingIpJob::dispatch();
+
     }
 }
