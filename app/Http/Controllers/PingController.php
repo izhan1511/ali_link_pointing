da9d3a33771ff9f85afed13e08203ad90ac21c79
izhan1511 <izhan47@gmail.com>
2022-10-07 21:08:45 +0500

    Update

--- app/Http/Controllers/PingController.php
+++ app/Http/Controllers/PingController.php
@@ -15,25 +15,26 @@ class PingController extends Controller
         $fourth = rand(0, 255);
         $ip = $first . '.' . $sec . '.' . $third . '.' . $fourth;
         try {
-            $check = ip::where('ip', $ip)->first();
-            if (!$ip) {
-                $ch = curl_init($ip);
-                // $ch = curl_init('1.1.1.1');
-                curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-                curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                $data = curl_exec($ch);
-                $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-                curl_close($ch);
 
-                if ($data !== false) {
+            $ch = curl_init($ip);
+            // $ch = curl_init('1.1.1.1');
+            curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $data = curl_exec($ch);
+            $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            curl_close($ch);
+
+            if ($data !== false) {
+                $check = ip::where('ip', $ip)->first();
+                if (!$ip) {
                     $ip = ip::create([
                         'ip' => $ip,
                     ]);
-                } else {
-                    // \Log::error('IP: ' . $ip);
-                    // \Log::error('data: ' . json_encode($data));
                 }
+            } else {
+                // \Log::error('IP: ' . $ip);
+                // \Log::error('data: ' . json_encode($data));
             }
 
         } catch (\exception $e) {
